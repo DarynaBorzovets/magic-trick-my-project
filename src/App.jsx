659952e4096e2b26fbfe710d28495e48e2e1b690
DarynaBorzovets <biz.daryna@gmail.com>
2024-03-20 13:38:33 -0400

    Created Vite, installeddependencies, Tailwind, react-router-dom, tested App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Play
+            </button>
+    </>
+  )
+}
+
+export default App
