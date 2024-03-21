91e021ff404147852b3053ed4cc901f1adb33d22
DarynaBorzovets <biz.daryna@gmail.com>
2024-03-21 09:59:55 -0400

    Routes created in the  App.jsx, page Focus created with button and some Tailwind.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import Gallery from './pages/Gallery'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css'
+import Focus from './pages/Focus';
+import Gallery from './pages/Gallery';
+import Result from './pages/Result';
+import Start from './pages/Start'; 
 
 function App() {
-
-  return (
-    <>
-   <Gallery/>
-    </>
-  )
+  return (<>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Focus />}/>
+        <Route path="/focus" element={<Focus/>}/>
+        <Route path="/gallery" element={<Gallery/>}/>
+        <Route path="/start" element={<Start />}/>
+        <Route path="/result" element={<Result />}/>
+      </Routes>
+    </BrowserRouter></>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Focus.jsx
+++ src/pages/Focus.jsx
@@ -1,10 +1,27 @@
+import { useNavigate } from 'react-router-dom';
 
 function Focus() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/gallery');
+  };
+
   return (
-    <div>
-      
+    <div className="bg-gradient-to-r from-indigo-500 to-emerald-500 min-h-screen overflow-hidden flex flex-col justify-center items-center">
+      <h1 className="text-6xl font-bold mb-4 text-center text-indigo-600 shadow-lg">Mind Game</h1>
+      <p className="text-3xl mb-8 text-center text-indigo-600 shadow-lg">Think about your card...<br/>Focus... Let the magic happen.</p>
+      <div>
+        <button
+          className="bg-indigo-600 hover:bg-indigo-700 text-emerald-500 font-bold py-2 px-4 rounded"
+          style={{ width: '150px' }}
+          onClick={handleClick}
+        >
+          Go
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Focus
+export default Focus;
